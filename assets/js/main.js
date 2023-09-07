23d633172d0431b7671af04417d2295ec712b5e8
happyxddd <252018@mck-ktits.ru>
2023-09-07 15:24:14 +0300

    sm2(js)

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let city_name = prompt ("Введите название города");
+let count_people = prompt ("Какое население в этом городе?");
+let city_age = prompt ("Введите год образования этого города");
+let year = 2023 ;
+let city_age_count = (year - city_age);
+alert ('Городу ' + city_name +' исполнилось '+ city_age_count + ' с момента его образовния. Население '+ count_people + ' человек' );
